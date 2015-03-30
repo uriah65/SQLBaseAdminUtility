4348c0d06ef308bfbdcc9ea8e3e17eb089c73562
Uriah65 <uriah65@users.noreply.github.com>
2015-03-29 20:22:01 -0400

    .

---
+++ SQLBaseAdmin/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBaseAdmin
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
